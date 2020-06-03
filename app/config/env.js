e0b249474393c2721471eac9feb3d6f94436ad2f
pejuangbitco <irsyadeinstein@gmail.com>
2020-06-03 09:24:25 +0700

    add heroku pg+credentials

--- app/config/env.js
+++ app/config/env.js
@@ -1,9 +1,9 @@
 	
 const env = {
-    database: 'crudnode',
-    username: 'root',
-    password: '',
-    host: 'localhost',
+    database: 'd93lgq6elcm7i4',
+    username: 'avoogibcjlvksf',
+    password: '2979011cef1e99240ada459c1515655c77e1df36f5afa9702d15424819f335ca',
+    host: 'ec2-34-232-147-86.compute-1.amazonaws.com',
     dialect: 'postgres',
     pool: {
       max: 5,
